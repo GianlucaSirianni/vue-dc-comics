76d5204f1d74e45aa9806ad690893fe86cc66b01
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2022-11-24 17:08:25 +0100

    terminato content, mancano buttons

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,47 @@
 <template>
   <div id="app">
-
     <HeaderComp/>
-
+    <JumbotronComp/>
     <ContentComp/>
-
     <MainNav/>
-
     <FooterNav/>
-
     <FooterSocial/>
-
-
 
   </div>
 </template>
 
 <script>
 import HeaderComp from './components/HeaderComp.vue'
+import JumbotronComp from './components/JumbotronComp.vue'
 import ContentComp from './components/ContentComp.vue'
 import MainNav from './components/MainNav.vue'
 import FooterNav from './components/FooterNav.vue'
 import FooterSocial from './components/FooterSocial.vue'
 
+
+
+
+
 export default {
   name: 'App',
   components: {
     HeaderComp,
+    JumbotronComp,
     ContentComp,
     MainNav,
     FooterNav,
     FooterSocial
+
+
   }
 }
 </script>
 
 <style lang="scss">
 #app {
+  width: 100vw;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  width: 100vw;
-
 }
 </style>
---
+++ src/components/ComicComp.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="comicCard">
+        <img :src=dettagliProdotto.thumb alt="">
+        <p>{{dettagliProdotto.series.toUpperCase()}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ComicComp',
+        props: {
+            dettagliProdotto: Object
+        }
+    }
+
+</script>
+
+<style lang="scss" scoped>
+.comicCard{
+        width: calc(100%/6);
+        color: white;
+    }
+</style>
